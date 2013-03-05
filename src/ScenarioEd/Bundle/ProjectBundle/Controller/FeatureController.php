553bd5a5d724843b25cc6b1a2384dec50d6f8c8a
Gregory Perry <lotyrin@gmail.com>
2013-03-05 12:45:17 -0800

    Initial Feature CRUD

--- src/ScenarioEd/Bundle/ProjectBundle/Controller/FeatureController.php
+++ src/ScenarioEd/Bundle/ProjectBundle/Controller/FeatureController.php
@@ -6,10 +6,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use ScenarioEd\Bundle\ProjectBundle\Entity\Project;
+use ScenarioEd\Bundle\ProjectBundle\Entity\Feature;
 use ScenarioEd\Bundle\ProjectBundle\Form\FeatureType;
-
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Exception\IOException;
 
 /**
  * Feature controller.
@@ -38,9 +37,179 @@ class FeatureController extends BaseController
             throw $this->createNotFoundException('Unable to find Project entity.');
         }
 
+        $deleteForm = $this->createDeleteForm($file);
+
         return array(
             'project' => $project,
             'feature' => $feature,
+            'delete_form' => $deleteForm->createView(),
         );
+    }
+
+    /**
+     * Displays a form to create a new Feature entity.
+     *
+     * @Route("/new", name="project_feature_new")
+     * @Template()
+     */
+    public function newAction($project_id)
+    {
+        $request = $this->getRequest();
+        $file = $request->query->get('file');
+
+        $em = $this->getDoctrine()->getManager();
+        $project = $em->getRepository('ScenarioEdProjectBundle:Project')->find($project_id);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $form = $this->createCreateForm($project_id, $file);
+
+        return array(
+            'form'   => $form->createView(),
+            'project' => $project,
+        );
+    }
+
+    /**
+     * Creates a new Feature entity.
+     *
+     * @Route("/create", name="project_feature_create")
+     * @Method("POST")
+     * @Template("ScenarioEdProjectBundle:Feature:new.html.twig")
+     */
+    public function createAction(Request $request, $project_id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $project = $em->getRepository('ScenarioEdProjectBundle:Project')->find($project_id);
+
+        $form = $this->createCreateForm($project_id);
+        $form->bind($request);
+
+        $data = $form->getData();
+        $entity  = new Feature($project->getRepositoryURI() . 'features/' . $data['file']);
+
+        if ($form->isValid()) {
+            $entity->create();
+            $entity->setContents("Feature: New feature\n");
+
+            return $this->redirect($this->generateUrl('project_show', array('id' => $project->getId())));
+        }
+
+        return array(
+            'project' => $project,
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Feature entity.
+     *
+     * @Route("/edit", name="project_feature_edit")
+     * @Template()
+     */
+    public function editAction($project_id)
+    {
+        $request = $this->getRequest();
+        $file = $request->query->get('file');
+
+        $em = $this->getDoctrine()->getManager();
+        $project = $em->getRepository('ScenarioEdProjectBundle:Project')->find($project_id);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $feature = new Feature($file);
+
+        $editForm = $this->createForm(new FeatureType(), $feature);
+        $deleteForm = $this->createDeleteForm($file);
+
+        return array(
+            'project'     => $project,
+            'feature'     => $feature,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Feature entity.
+     *
+     * @Route("/update", name="project_feature_update")
+     * @Method("POST")
+     * @Template("ScenarioEdProjectBundle:Feature:edit.html.twig")
+     */
+    public function updateAction(Request $request, $project_id)
+    {
+        $request = $this->getRequest();
+        $file = $request->query->get('file');
+
+        $em = $this->getDoctrine()->getManager();
+        $project = $em->getRepository('ScenarioEdProjectBundle:Project')->find($project_id);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $feature = new Feature($file);
+
+        $deleteForm = $this->createDeleteForm($file);
+        $editForm = $this->createForm(new FeatureType(), $feature);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            // Do update.
+
+            return $this->redirect($this->generateUrl('project_feature_show', array('project_id' => $project_id, 'file' => $file)));
+        }
+
+        return array(
+            'feature'      => $feature,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Feature entity.
+     *
+     * @Route("/delete", name="project_feature_delete")
+     * @Method("POST")
+     */
+    public function deleteAction(Request $request, $project_id)
+    {
+        $request = $this->getRequest();
+        $file = $request->query->get('file');
+
+        $form = $this->createDeleteForm($file);
+        $form->bind($request);
+
+        $feature = new Feature($file);
+
+
+        if ($form->isValid()) {
+            $feature->delete();
+        }
+
+        return $this->redirect($this->generateUrl('project_feature_show', array('project_id' => $project_id, 'file' => $file)));
+    }
+
+    private function createDeleteForm($file)
+    {
+        return $this->createFormBuilder(array('file' => $file))
+            ->add('file', 'hidden')
+            ->getForm()
+        ;
+    }
+
+    private function createCreateForm()
+    {
+        return $this->createFormBuilder()
+            ->add('file', 'text')
+            ->getForm()
+        ;
     }
 }
---
+++ src/ScenarioEd/Bundle/ProjectBundle/Form/FeatureType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ScenarioEd\Bundle\ProjectBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FeatureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('contents', 'textarea', array('attr' => array('rows' => 25, 'cols' => 80)))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ScenarioEd\Bundle\ProjectBundle\Entity\Feature'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'scenarioed_projectbundle_featuretype';
+    }
+}
