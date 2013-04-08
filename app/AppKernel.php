5dc089368b1da98446ac8b606dee4b9daa25e344
Melissa Anderson <melissa@happypunch.com>
2013-04-07 23:23:44 -0400

    The rest of the files for the Help bundle are present

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new ScenarioEd\Bundle\ProjectBundle\ScenarioEdProjectBundle(),
+            new ScenarioEd\HelpBundle\ScenarioEdHelpBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
